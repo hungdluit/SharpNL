28523591f251431ec51da0ba23f39f23309622d6
knuppe <gustavoknuppe@gmail.com>
2015-02-19 20:22:29 -0200

    Ops! I had forgotten to port this one... :D

--- src/SharpNL/NameFind/NameSampleSequenceStream.cs
+++ src/SharpNL/NameFind/NameSampleSequenceStream.cs
@@ -1,64 +1,108 @@
-﻿using System;
+﻿// 
+//  Copyright 2014 Gustavo J Knuppe (https://github.com/knuppe)
+// 
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+// 
+//       http://www.apache.org/licenses/LICENSE-2.0
+// 
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// 
+//   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//   - May you do good and not evil.                                         -
+//   - May you find forgiveness for yourself and forgive others.             -
+//   - May you share freely, never taking more than you give.                -
+//   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//  
+
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using SharpNL.ML.Model;
 using SharpNL.Utility;
 using SharpNL.Utility.FeatureGen;
 using Sequence = SharpNL.ML.Model.Sequence;
 
 namespace SharpNL.NameFind {
+
+    /// <summary>
+    /// Represents a <seealso cref="NameSample"/> sequence stream used to train the name finder models.
+    /// </summary>
     public class NameSampleSequenceStream : ISequenceStream {
+        private readonly bool useOutcomes;
+        private readonly INameContextGenerator pcg;
+        private readonly IObjectStream<NameSample> psi;
+        private readonly ISequenceCodec<String> seqCodec;
 
-          private INameContextGenerator pcg;
-          private readonly bool useOutcomes;
-          private IObjectStream<NameSample> psi;
-          private ISequenceCodec<String> seqCodec;
+        #region + Constructors .
 
-
-        /*
-        public NameSampleSequenceStream(IObjectStream<NameSample> psi)
-            : this(psi, new DefaultNameContextGenerator(), true) {
-            
-        }
-        */
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NameSampleSequenceStream"/> class.
+        /// </summary>
+        /// <param name="psi">The sample stream.</param>
+        /// <param name="featureGen">The feature generator.</param>
         public NameSampleSequenceStream(IObjectStream<NameSample> psi, IAdaptiveFeatureGenerator featureGen)
-            : this(psi, new DefaultNameContextGenerator(featureGen), true) {
-            
-        }
+            : this(psi, new DefaultNameContextGenerator(featureGen), true) {}
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NameSampleSequenceStream"/> class.
+        /// </summary>
+        /// <param name="psi">The sample stream.</param>
+        /// <param name="featureGen">The feature generator.</param>
+        /// <param name="useOutcomes">if set to <c>true</c> will be used in the samples.</param>
         public NameSampleSequenceStream(IObjectStream<NameSample> psi, IAdaptiveFeatureGenerator featureGen,
-            bool useOutcomes) : this(psi, new DefaultNameContextGenerator(featureGen), useOutcomes) {
-            
-        }
+            bool useOutcomes) : this(psi, new DefaultNameContextGenerator(featureGen), useOutcomes) {}
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NameSampleSequenceStream"/> class.
+        /// </summary>
+        /// <param name="psi">The sample stream.</param>
+        /// <param name="pcg">The context generator.</param>
         public NameSampleSequenceStream(IObjectStream<NameSample> psi, INameContextGenerator pcg)
-            : this(psi, pcg, true) {
-            
-        }
+            : this(psi, pcg, true) {}
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NameSampleSequenceStream"/> class using the <seealso cref="BioCodec"/> as sequence codec.
+        /// </summary>
+        /// <param name="psi">The sample stream.</param>
+        /// <param name="pcg">The context generator.</param>
+        /// <param name="useOutcomes">if set to <c>true</c> will be used in the samples.</param>
         public NameSampleSequenceStream(IObjectStream<NameSample> psi, INameContextGenerator pcg, bool useOutcomes)
-            : this(psi, pcg, useOutcomes, new BioCodec()) {
-            
-        }
+            : this(psi, pcg, useOutcomes, new BioCodec()) {}
 
-        public NameSampleSequenceStream(IObjectStream<NameSample> psi, INameContextGenerator pcg, bool useOutcomes, ISequenceCodec<string> seqCodec) {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NameSampleSequenceStream"/> class.
+        /// </summary>
+        /// <param name="psi">The sample stream.</param>
+        /// <param name="pcg">The context generator.</param>
+        /// <param name="useOutcomes">if set to <c>true</c> will be used in the samples.</param>
+        /// <param name="seqCodec">The sequence codec.</param>
+        public NameSampleSequenceStream(IObjectStream<NameSample> psi, INameContextGenerator pcg, bool useOutcomes,
+            ISequenceCodec<string> seqCodec) {
             this.psi = psi;
             this.useOutcomes = useOutcomes;
             this.pcg = pcg;
             this.seqCodec = seqCodec;
         }
 
+        #endregion
 
+        #region + Properties .
+
+        #region . Dispose .
         /// <summary>
         /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
         /// </summary>
         public void Dispose() {
-            throw new NotImplementedException();
+            psi.Dispose();
         }
+        #endregion
 
+        #region . Read .
         /// <summary>
         /// Returns the next object. Calling this method repeatedly until it returns,
         /// null will return each object from the underlying source exactly once.
@@ -67,18 +111,45 @@ namespace SharpNL.NameFind {
         /// The next object or null to signal that the stream is exhausted.
         /// </returns>
         public Sequence Read() {
-            throw new NotImplementedException();
-        }
+            var sample = psi.Read();
+            if (sample != null) {
 
+                var events = new Event[sample.Sentence.Length];
+
+                for (int i = 0; i < sample.Sentence.Length; i++) {
+
+                    // it is safe to pass the tags as previous tags because
+                    // the context generator does not look for non predicted tags
+                    var tags = seqCodec.Encode(sample.Names, sample.Sentence.Length);
+
+
+                    var context = pcg.GetContext(
+                        i,
+                        sample.Sentence,
+                        useOutcomes ? tags : null,
+                        null);
+
+                    events[i] = new Event(tags[i], context);
+                }
+
+                return new Sequence(events, sample);
+            }
+            return null;
+        }
+        #endregion
+
+        #region . Reset .
         /// <summary>
         /// Repositions the stream at the beginning and the previously seen object 
         /// sequence will be repeated exactly. This method can be used to re-read the
         /// stream if multiple passes over the objects are required.
         /// </summary>
         public void Reset() {
-            throw new NotImplementedException();
+            psi.Reset();
         }
+        #endregion
 
+        #region . UpdateContext .
         /// <summary>
         /// Creates a new event array based on the outcomes predicted by the specified parameters for the specified sequence.
         /// </summary>
@@ -86,20 +157,19 @@ namespace SharpNL.NameFind {
         /// <param name="model">The model.</param>
         /// <returns>The event array.</returns>
         public Event[] UpdateContext(Sequence sequence, AbstractModel model) {
+            var tagger =
+                new NameFinderME(
+                    new TokenNameFinderModel("x-unspecified", model, new Dictionary<string, object>(), null));
 
-            /*
-               var tagger = new NameFinderME(new TokenNameFinderModel("x-unspecified", model, new Dictionary<string, string>(), null));
+            var sentence = sequence.GetSource<NameSample>().Sentence;
 
-               String[] sentence = sequence.GetSource<NameSample>().Sentence;
+            var tags = seqCodec.Encode(tagger.Find(sentence), sentence.Length);
 
-    String[] tags = seqCodec.Encode(tagger.Find(sentence), sentence.Length);
-    Event[] events = new Event[sentence.length];
-
-            NameFinderEventStream.GenerateEvents(sentence,tags,pcg).toArray(events);
-
-    return events;
-            */
-            throw new NotImplementedException();
+            return NameFinderEventStream.GenerateEvents(sentence, tags, pcg).ToArray();
         }
+        #endregion
+
+        #endregion
+
     }
 }
